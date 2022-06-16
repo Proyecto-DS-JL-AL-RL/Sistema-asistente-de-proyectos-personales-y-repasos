0415dd602cd810725cbb83e46b131d0cb2befbed
Jorge Parishuana <jorge.parishuana.o@uni.pe>
2022-06-16 12:18:48 -0500

    setup0

---
+++ proyectSrc/react-front/src/components/Component1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Typography } from '@mui/material';
+
+export default function Component1(){
+
+    return (
+        <React.Fragment>
+            <Typography variant = 'h1'>
+                Componente 1
+            </Typography>
+            <Typography variant = 'h3'>
+                Componente 1
+            </Typography>
+            <Typography variant = 'h4'>
+                Componente 1
+            </Typography>
+        </React.Fragment>
+    );
+}
