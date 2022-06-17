95416eac6654b6671486f3121b4c0cf84cb112c9
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2022-06-17 14:41:02 -0500

    HorarioEsqueleto

--- proyectSrc/react-front/src/App.js
+++ proyectSrc/react-front/src/App.js
@@ -3,24 +3,27 @@ import {Switch , Route} from 'react-router-dom';
 import Page1 from './pages/Page1';
 import AppBarSearch from './components/AppBarSearch';
 import DrawerComponent from './components/DrawerComponent';
+import Horario from './pages/Horario';
 
 function App() {
   return (
     <div className='container-main'>
       <DrawerComponent/>
-    <div className='other-container'>
-      <div className='head-container'>
-        <AppBarSearch/>
+      <div className='other-container'>
+        <div className='head-container'>
+          <AppBarSearch/>
+        </div>
+        <div className='content-container'>
+          <Switch>
+            <Route path = '/' exact>
+              <Page1/>
+            </Route>
+            <Route path = '/horario'>
+              <Horario/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <div className='content-container'>
-        contenido
-        <Switch>
-          <Route path = '/'>
-            <Page1/>
-          </Route>
-        </Switch>
-      </div>
-    </div>
   </div>
   );
 }
