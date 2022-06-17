efdb14a6eae11b7096237d3d75a33efef2400cbd
Alexander <alexander.lique.l@uni.pe>
2022-06-17 01:39:35 -0500

    se añadió Inicio, Mazos

--- proyectSrc/react-front/src/components/Component1.js
+++ proyectSrc/react-front/src/components/Component1.js
@@ -1,5 +1,22 @@
 import React from "react";
 import { Typography } from '@mui/material';
+import CustomizedAccordions from './componentesALL/Acordion'
+const mazos = [{
+    id: '1',
+    titulo: 'Calidad de Software',
+    descripcion: 'esto es un prueba porque estamos a punto de dar parcial de calidad de software pero no estudie asi que es tu turno de colaborar.'
+  },
+  {
+    id:'2',
+    titulo: 'Inteligencia Artificial',
+    descripcion: 'esto es un prueba porque estamos a punto de dar parcial de AI pero no estudie asi que es tu turno de colaborar.'
+  }, {
+    id:'3',
+    titulo: 'Interaccion humano computador',
+    descripcion: 'esto es una prueba que no ira algun lado xd por ede mueres aaaa'
+  }
+  ]
+
 
 export default function Component1(){
 
@@ -14,6 +31,7 @@ export default function Component1(){
             <Typography variant = 'h4'>
                 Componente 1
             </Typography>
+            <CustomizedAccordions getmazo={mazos}/>
         </React.Fragment>
     );
 }
---
+++ proyectSrc/react-front/src/pages/Inicio.js
@@ -0,0 +1,9 @@
+import InputAdornments from '../components/componentesALL/login'
+import React from "react";
+
+export default function Component1(){
+
+    return (
+          <InputAdornments/>  
+        );
+    }
