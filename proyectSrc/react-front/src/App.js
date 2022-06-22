8d5f79029845fdab83747f707ec1e28a948c1c90
Jorge Parishuana <jorge.parishuana.o@uni.pe>
2022-06-22 09:19:51 -0500

    Direccionamiento 01

--- proyectSrc/react-front/src/App.js
+++ proyectSrc/react-front/src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
-import {Switch , Route} from 'react-router-dom';
+import {Switch , Route, useLocation, useHistory} from 'react-router-dom';
 import Page1 from './pages/Page1';
 import AppBarSearch from './components/AppBarSearch';
 import DrawerComponent from './components/DrawerComponent';
 import Inicio from './pages/Inicio';
 import ProyectoView from './pages/projectDashboard';
 import AlgoQueHacer from './pages/AlgoQueHacer';
+import SR,{useSpeechRecognition} from 'react-speech-recognition';
+import { Beforeunload } from 'react-beforeunload';
+import {getCommands} from './util/redirectSpeech';
+import { useState } from 'react';
 
 function App() {
+  const CONTINOUS_ = false;
+
+
+  const location = useLocation();
+  const history = useHistory();
+  const [listeningState,setListeningState] = useState(false);
+  const commands = getCommands(location,history);
+  const {listening} = useSpeechRecognition({commands:commands});
+  const beforeUnload = ()=>{
+    if (listening)
+      SR.stopListening();
+  }
+
+  document.addEventListener("visibilitychange", event => {
+    if (listeningState){
+    if (document.visibilityState === "visible") {
+      SR.startListening({language: 'es', continuous: CONTINOUS_});
+    } else {
+      SR.stopListening();
+    }
+    }else{
+      SR.stopListening(); 
+    }
+  })
+
+
+
   return (
     <div className='container-main'>
+      <button onClick = {()=>{listening?SR.stopListening():SR.startListening({language: 'es', continuous: CONTINOUS_});setListeningState(!listeningState)}}>xd</button>
       <DrawerComponent/>
     <div className='other-container'>
       <div className='head-container'>
@@ -32,6 +64,7 @@ function App() {
         </Switch>
       </div>
     </div>
+    <Beforeunload onBeforeunload= {beforeUnload} />
   </div>
   );
 }
---
+++ proyectSrc/react-front/src/util/redirectSpeech.js
@@ -0,0 +1,35 @@
+
+const paths = {
+    "inicio": "/inicio",
+    "dame algo que hacer":"/algoQueHacer",
+    "darme algo que hacer":"/algoQueHacer",
+    "tarjetas":"/",
+    "mazos":"/",
+    "cartas":"/",
+    "proyecto":"/proyect",
+    "Dashboard":"/proyect"
+}
+
+
+
+const getCommands = (location,history) =>{
+    const comando = [{
+        command: ["Llévame a *."],
+        callback: (frase) =>{
+            alert(frase);
+            const path = paths[frase]
+            if (path != null && path != undefined){
+                if (path != location.pathname){
+                    history.push(path);
+                }                   
+                else    
+                    alert("Ya se encuentra en esa pagina");                
+            }            
+        }
+    }]
+
+    return comando;
+}
+
+
+export {getCommands}
