dd1fe02d1a7f8d8bfdde3d20645e56e115c3476d
Alexander <alexander.lique.l@uni.pe>
2022-06-26 10:41:53 -0500

    se arreglo el login

--- proyectSrc/react-front/src/pages/Inicio.js
+++ proyectSrc/react-front/src/pages/Inicio.js
@@ -11,7 +11,7 @@ import Grid from '@mui/material/Grid';
 //<MostrarFuncionalidades/>
 
 const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(20),
+  padding: theme.spacing(15),
   textAlign: 'center',
 }));
 
@@ -21,19 +21,20 @@ export default function Inicio(){
     return (
       <React.Fragment>
           <Box sx={{ flexGrow: 1 }}>
-            <Grid container justifyContent="center" rowSpacing={1} columnSpacing={{ xs: 1, sm: 45, md: 10 }}>
-            <Grid item xs={5.5} mt={45} >
+            <Grid container justifyContent="center" rowSpacing={2} columnSpacing={{ xs: 1, sm: 5, md: 10 }}>
+            <Grid item xs={3} mt={45}  md = {10} sm={10} lg ={4} xl = {4.5}>
                     <ThemeProvider theme={darkTheme}>
                           <Item ><Login/></Item>
                     </ThemeProvider>
                 </Grid>
-              <Grid item xs={1}>
-                </Grid>
+              <Grid item xs={6.5}  mt={20}>
                     <img   
-                          src={"https://drive.google.com/uc?export=view&id=1e9TrTH56TwOvOuKBPzIwfEuZwrz605sn"}
-                                            width= "45%" 
-                                            height="45%"
-                                            />
+                        src={"https://drive.google.com/uc?export=view&id=1e9TrTH56TwOvOuKBPzIwfEuZwrz605sn"}
+                                                  width= "100%" 
+                                                  height="100%"
+                                                  />
+
+                </Grid>
             </Grid>
         </Box>
       </React.Fragment>
