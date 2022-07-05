e1f1cc09f1bbc1f781e5e03feaf887fccb3ddbff
Alexander <alexander.lique.l@uni.pe>
2022-07-05 15:44:37 -0500

    se modifico la primera parte de la presentación

--- proyectSrc/react-front/src/pages/Presentation.js
+++ proyectSrc/react-front/src/pages/Presentation.js
@@ -8,48 +8,78 @@ import Slide from '@mui/material/Slide';
 import Grid from '@mui/material/Grid';
 import MicIcon from '@mui/icons-material/Mic';
 import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
-import CardContent from '@mui/material/CardContent';
-import Card from '@mui/material/Card';
-import Grow from '@mui/material/Grow';
+//import CardContent from '@mui/material/CardContent';
+//import Card from '@mui/material/Card';
+//import Grow from '@mui/material/Grow';
 import { useHistory } from "react-router-dom";
 
 //vw ch %
+/*
+const [showRecomendacion, setShowRecomendacion] = useState(false)
+
+{showRecomendacion?
+                <Grow  timeout={1000}  in={showRecomendacion}>
+                    <Box mt={40}  position={'absolute'} justifyContent="center" sx={{ display: 'flex', flexWrap: 'wrap' }}> 
+                        <Card  sx={{mx:100, minWidth: 600, border: '0.5px solid purple'  }}>
+                                <CardContent>
+                                    
+                                    <Typography mt={10} sx={{fontWeight: 'bold', textAlign: 'center'}} variant="h4" component="div">
+                                                    Este es un mensaje de ayuda, recomendación, motivación
+                                    </Typography>
+                                    <Box  justifyContent="center" sx={{p:12, display: 'flex', flexWrap: 'wrap' }}> 
+                                                <Button onClick={()=>{history.push('/')}} sx={{borderRadius:50, p:4,                            
+                                                                    width: 200, height: 85,
+                                                                    color:'white', background:'#00b347',
+                                                                    fontSize:30, 
+                                                                    '&:hover': {backgroundColor: '#cfe619'}
+                                                                    }}>Aceptar</Button>
+                                    </Box>        
+                                </CardContent>
+                            </Card>
+                    </Box>
+                </Grow>
+                :null}
+*/
 export default function Presentacion(){
     let history = useHistory()
     const [showPresentacion, setShowPresentacion] = useState(true)
-    const [showRecomendacion, setShowRecomendacion] = useState(false)
     return (
         <React.Fragment>     
                 {showPresentacion?
                     <React.Fragment> 
-                            <Box mt={50} position={'static'} display="flex" justifyContent="center">
+                            <Box mt={'15%'} mx={'25%'} textAlign={"center"} position={'absolute'} display="flex" flexWrap={'wrap'} justifyContent="center">
                                 <Stack spacing={3}>
                                         <Zoom  timeout={1000} in={true}>
                                                 <Typography sx={{fontWeight: 'bold'}}  variant = 'h3'>
-                                                    Presentación de la Aplicación
+                                                    Bienvenido a nuestra Aplicación
                                                 </Typography>
                                         </Zoom>
                                         <Zoom  timeout={1000} in={true}>
-                                                <Typography justifyContent="center" sx={{fontWeight: 'bold', textAlign: 'center'}}  variant = 'h3'>
+                                                <Typography textAlign={"center"} justifyContent="center" sx={{fontWeight: 'bold', textAlign: 'center'}}  variant = 'h3'>
                                                     Usuario 1
                                                 </Typography>
                                         </Zoom>
-                                </Stack>
+                                            <Box  sx={{
+                                            mt:'45%', 
+                                            textAlign:"center",
+                                            flexWrap:'wrap',
+                                            position: 'static'}} display="flex" justifyContent="center">
+                                                <Zoom  timeout={1000} in={true}>
+                                                    <Button onClick={()=>{
+                                                        setShowPresentacion(false)
+                                                    }}  sx={{borderRadius:'10%', p:'2%',                            
+                                                                width: '15ch', height: '5ch',
+                                                                fontWeight: 'bold',
+                                                                color:'white', background:'#00b347',
+                                                                fontSize:30, 
+                                                                '&:hover': {backgroundColor: '#cfe619', color:'#808080'}
+                                                                }} variant="contained" size="large">
+                                                        <Typography sx={{fontWeight: 'bold'}} variant = 'h5'>Continuar</Typography></Button>
+                                                </Zoom>
+                                            </Box>
+                                    </Stack>
                             </Box>
-                            <Box mt={5} sx={{
-                                        position: 'static'}} display="flex" justifyContent="center">
-                                        <Zoom  timeout={1000} in={true}>
-                                                <Button onClick={()=>{
-                                                    setShowPresentacion(false)
-                                                }} sx={{borderRadius:50, p:2,                            
-                                                            width: 200, height: 85,
-                                                            color:'white', background:'#00b347',
-                                                            fontSize:30, 
-                                                            '&:hover': {backgroundColor: '#cfe619'}
-                                                            }} variant="contained" size="large">
-                                                    <Typography sx={{fontWeight: 'bold'}} variant = 'h5'>Empezar</Typography></Button>
-                                        </Zoom>
-                            </Box>
+                            
                     </React.Fragment>
                     :
                         <React.Fragment> 
@@ -82,40 +112,19 @@ export default function Presentacion(){
                                         <Slide direction="up" timeout={1000} in={true}  mountOnEnter unmountOnExit>
                                                 <Button onClick= {()=>{
                                                                 setShowPresentacion(false)
-                                                                setShowRecomendacion(true)
+                                                                history.push('/')
                                                             }} sx={{borderRadius:50, p:4,                            
                                                             width: 200, height: 85,
                                                             color:'white', background:'#00b347',
                                                             fontSize:30, 
                                                             '&:hover': {backgroundColor: '#cfe619'}
                                                             }} variant="contained" size="large">
-                                                    <Typography sx={{fontWeight: 'bold'}} variant = 'h5'>Continuar</Typography></Button>
+                                                    <Typography sx={{fontWeight: 'bold'}} variant = 'h5'>Empezemos </Typography></Button>
                                         </Slide>
                                 </Box>
                         </React.Fragment>
             }
-            {showRecomendacion?
-                <Grow  timeout={1000}  in={showRecomendacion}>
-                    <Box mt={40}  position={'absolute'} justifyContent="center" sx={{ display: 'flex', flexWrap: 'wrap' }}> 
-                        <Card  sx={{mx:100, minWidth: 600, border: '0.5px solid purple'  }}>
-                                <CardContent>
-                                    
-                                    <Typography mt={10} sx={{fontWeight: 'bold', textAlign: 'center'}} variant="h4" component="div">
-                                                    Este es un mensaje de ayuda, recomendación, motivación
-                                    </Typography>
-                                    <Box  justifyContent="center" sx={{p:12, display: 'flex', flexWrap: 'wrap' }}> 
-                                                <Button onClick={()=>{history.push('/')}} sx={{borderRadius:50, p:4,                            
-                                                                    width: 200, height: 85,
-                                                                    color:'white', background:'#00b347',
-                                                                    fontSize:30, 
-                                                                    '&:hover': {backgroundColor: '#cfe619'}
-                                                                    }}>Aceptar</Button>
-                                    </Box>        
-                                </CardContent>
-                            </Card>
-                    </Box>
-                </Grow>
-                :null}
+            
         </React.Fragment>
     );
 }
