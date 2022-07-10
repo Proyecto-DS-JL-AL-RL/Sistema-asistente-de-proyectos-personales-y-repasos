e93a778310d770c5386c025ff7708e4a69edd519
Alexander <alexander.lique.l@uni.pe>
2022-07-10 18:23:37 -0500

    key

--- proyectSrc/expressApi/database.js
+++ proyectSrc/expressApi/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 
-const mongoDB = 'mongodb+srv://Alexander_Lique:7nDUGXhFLPUKMk1Y@cluster0.oni4k.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://Alexander_Lique:<password>@cluster0.oni4k.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(db => console.log('BDConnected'))
 .catch(err=>console.log(err));
