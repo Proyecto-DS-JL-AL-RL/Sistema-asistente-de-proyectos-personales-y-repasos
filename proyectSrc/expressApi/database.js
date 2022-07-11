f0727456756b640801f37457aa0bfd4c0bd27694
JorgeParishuana <84486514+JorgeParishuana@users.noreply.github.com>
2022-07-11 08:27:30 -0500

    secret

--- proyectSrc/expressApi/database.js
+++ proyectSrc/expressApi/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 
-const mongoDB = 'mongodb+srv://react_client:ReactClient123@cluster0.oni4k.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://<user>:<cont>@cluster0.oni4k.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(db => console.log('BDConnected'))
 .catch(err=>console.log(err));
