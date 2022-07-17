79466b0fbc4a65fe1f00b1b96016be7a730d7d93
Alexander <alexander.lique.l@uni.pe>
2022-07-17 12:28:54 -0500

    se mejoro la esticia de los mazos y tambien se agrego la lógica de estos

--- proyectSrc/react-front/src/pages/verTarjeta.js
+++ proyectSrc/react-front/src/pages/verTarjeta.js
@@ -4,9 +4,9 @@ import Paper from '@mui/material/Paper';
 import Pagination from '@mui/material/Pagination';
 import { Button, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
-//import MicIcon from '@mui/icons-material/Mic';
+import MicIcon from '@mui/icons-material/Mic';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
+//import { styled } from '@mui/material/styles';
 import './funcionalidades.css'
 import Grow from '@mui/material/Grow';
 import Card from '@mui/material/Card';
@@ -21,6 +21,9 @@ import { changeContent,restoreContent } from '../stores/sliceAyuda';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import './Tarjetas.css'
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 //<MicIcon className='button-main' sx={{p:2, borderRadius:'50%', background:'red',
 //color:'white', width: '30%', height: '30%', '&:hover': {backgroundColor: '#FF6347'} }}/>
 
@@ -36,6 +39,10 @@ const mazos = {
   }]
 }
 
+function handlePosition(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
 
 export default function VerTarjeta(props) {
   const [page, setPage] = React.useState(1);
@@ -48,9 +55,33 @@ export default function VerTarjeta(props) {
   const [disabledB, setDisabledB] = React.useState("")
   const [disabledC, setDisabledC] = React.useState("")
   const [disabledD, setDisabledD] = React.useState("")
- 
+  const breadcrumbs = [
+    <Link underline="hover" key="1" color="inherit" href="/" onClick={handlePosition}>
+      MUI
+    </Link>,
+    <Link
+      underline="hover"
+      key="2"
+      color="inherit"
+      href="/material-ui/getting-started/installation/"
+      onClick={handlePosition}
+    >
+      Core
+    </Link>,
+    <Typography key="3" color="text.primary">
+      Breadcrumb
+    </Typography>,
+  ]; 
   const {idSeccion} = useParams();
   const handleChange = (event, value) => {
+    setDisabledA("")
+    setDisabledB("")
+    setDisabledC("")
+    setDisabledD("")
+    setColorA("#BBE7FE")
+    setColorB("#BBE7FE")
+    setColorC("#BBE7FE")
+    setColorD("#BBE7FE")
     setPage(value);
   };
   const dispatch = useDispatch();
@@ -91,19 +122,24 @@ export default function VerTarjeta(props) {
       <Typography sx={{fontWeight: 'bold'}} variant = 'h3'>
                   {mazo.Titulo}
       </Typography>
-      
-    <Box
-      sx={{
-        justifyContent: 'center',
-        display: 'flex',
-      }}
-    > 
+      <Breadcrumbs
+        separator={<NavigateNextIcon fontSize="small" />}
+        aria-label="breadcrumb"
+      >
+        {breadcrumbs}
+      </Breadcrumbs>
+      <Box
+        sx={{
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      > 
     <Grow in={true}>
                 <Paper sx={{
                     mx:'4%',  
                     position: 'absolute',        
                     width: '86%',
-                    height: '80%',
+                    height: '75%',
                     color:'yellow',
                     background:'#c8a2c8',
                     border: '3px solid black',
@@ -139,7 +175,7 @@ export default function VerTarjeta(props) {
                                                         direction="column" 
                                                         justifyContent="center">      
                                                       
-                                                      <Paper sx={{mt:'2%',    
+                                                      <Paper sx={{mt:'3%',    
                                                                 width: '80%',
                                                                 height: '12vw',
                                                                 position: 'relative',
@@ -157,25 +193,34 @@ export default function VerTarjeta(props) {
                                                                                     //console.log(mazo.Tarjetas[page-1].Respuesta-1)
                                                                                     if(mazo.Tarjetas[page-1].Opciones[mazo.Tarjetas[page-1].Respuesta-1] === mazo.Tarjetas[page-1].Opciones[0]){
                                                                                       setColorA('#008000')
+                                                                                      setDisabledA("disabled")
+                                                                                      setDisabledB("disabled")
+                                                                                      setDisabledC("disabled")
+                                                                                      setDisabledD("disabled")
                                                                                     }else{
                                                                                       setColorA('#ff0000')
+                                                                                      setDisabledA("disabled")
                                                                                     }
-                                                                                    setDisabledA("disabled")
+                                                                                    
                                                                                   }}  sx={{maxHeight:'20ch', height:'18ch', maxwidth:'70ch', width: '70ch', border: '5px solid black',color: 'black',background: colorA, '&:hover': {backgroundColor: '#0088b6'}}} disabled={disabledA}>
                                                                                       <Typography sx={{fontWeight: 'bold'}} variant="h6" component="div"> a. {mazo.Tarjetas[page-1].Opciones[0]}</Typography></Button>
                                                                                   <Button onClick={()=>{
                                                                                     if(mazo.Tarjetas[page-1].Opciones[mazo.Tarjetas[page-1].Respuesta-1] === mazo.Tarjetas[page-1].Opciones[2]){
                                                                                       setColorB('#008000')
+                                                                                      setDisabledA("disabled")
+                                                                                      setDisabledB("disabled")
+                                                                                      setDisabledC("disabled")
+                                                                                      setDisabledD("disabled")
                                                                                     }else{
                                                                                       setColorB('#ff0000')
+                                                                                      setDisabledB("disabled")
                                                                                     }
-                                                                                    setDisabledB("disabled")
                                                                                   }} className='button-main'  sx={{maxHeight:'20ch', height:'18ch', maxwidth:'70ch', width: '70ch', border: '5px solid black', color: 'black',background: colorB, '&:hover': {backgroundColor: '#0088b6'}}} disabled={disabledB}>
                                                                                       <Typography sx={{fontWeight: 'bold'}}  variant="h6" component="div">b. {mazo.Tarjetas[page-1].Opciones[2]}</Typography></Button>
                                                                               </Stack>
                                                                   </Grid>
                                                                   <Grid mt={'10%'} item xs={'10%'}>
-                                                                              
+                                                                              <MicIcon sx={{p:2,  borderRadius:50, background:'red', color:'white', width: 100, height: 100 }} size="large" />
                                                                   </Grid>
                                                                   <Grid item >
                                                                           <Box sx={{ width: '100%' }}>
@@ -183,18 +228,26 @@ export default function VerTarjeta(props) {
                                                                                   <Button className='button-main' onClick={()=>{
                                                                                     if(mazo.Tarjetas[page-1].Opciones[mazo.Tarjetas[page-1].Respuesta-1] === mazo.Tarjetas[page-1].Opciones[1]){
                                                                                       setColorC('#008000')
+                                                                                      setDisabledA("disabled")
+                                                                                      setDisabledB("disabled")
+                                                                                      setDisabledC("disabled")
+                                                                                      setDisabledD("disabled")
                                                                                     }else{
                                                                                       setColorC('#ff0000')
+                                                                                      setDisabledC("disabled")
                                                                                     }
-                                                                                    setDisabledC("disabled")
                                                                                   }} sx={{maxHeight:'20ch', height:'18ch', maxwidth:'70ch', width: '70ch',border: '5px solid black', color: 'black',background: colorC, '&:hover': {backgroundColor: '#0088b6'}}} disabled={disabledC}><Typography sx={{fontWeight: 'bold'}} variant="h6" component="div">c. {mazo.Tarjetas[page-1].Opciones[1]}</Typography></Button>
                                                                                   <Button className='button-main' onClick={()=>{
                                                                                     if(mazo.Tarjetas[page-1].Opciones[mazo.Tarjetas[page-1].Respuesta-1] === mazo.Tarjetas[page-1].Opciones[3]){
                                                                                       setColorD('#008000')
+                                                                                      setDisabledA("disabled")
+                                                                                      setDisabledB("disabled")
+                                                                                      setDisabledC("disabled")
+                                                                                      setDisabledD("disabled")
                                                                                     }else{
                                                                                       setColorD('#ff0000')
+                                                                                      setDisabledD("disabled")
                                                                                     }
-                                                                                    setDisabledD("disabled")
                                                                                   }} sx={{maxHeight:'20ch', height:'18ch', maxwidth:'70ch', width: '70ch', border: '5px solid black',  color: 'black',background: colorD, '&:hover': {backgroundColor: '#0088b6'}}} disabled={disabledD}><Typography sx={{fontWeight: 'bold'}} variant="h6" component="div">d. {mazo.Tarjetas[page-1].Opciones[3]}</Typography></Button>
                                                                               </Stack>
                                                                           </Box>
@@ -209,12 +262,12 @@ export default function VerTarjeta(props) {
     </Box>
     <Box
       sx={{
-        mt:'43%',
+        mt:'2.5%',
         justifyContent: 'center',
         display: 'flex',
       }}
     >
-        <Pagination sx={{textAlign: 'center'}} count={mazo.Tarjetas.length} page={page} onChange={handleChange}  color="secondary" />
+        <Pagination sx={{textAlign: 'center'}} count={mazo.Tarjetas.length} page={page} onChange={handleChange}  color="secondary" size="large"/>
     </Box>
     </React.Fragment>
   );
