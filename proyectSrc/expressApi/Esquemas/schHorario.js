9372430eb3d0ca72c349164fc38d3f29b3208325
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2022-07-20 01:02:48 -0500

    BackendHorario

---
+++ proyectSrc/expressApi/Esquemas/schHorario.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const configHorario = new Schema({
+    userSub: String,
+    intervaloDefault : {type: Boolean, default:false},
+    tema: {type: Number, default:1},
+    sobrescribir: {type:Boolean, default:true},
+    intervalo:{type:Array,default:[6,18]}
+})
+
+const horario = new Schema({
+    userSub: String,
+    horario: [],
+    config:{type: Schema.Types.ObjectId,ref:'ConfigHorario'}
+})
+const ConfigHorario = mongoose.model('ConfigHorario',configHorario);
+const Horario = mongoose.model('Horario',horario);
+
+module.exports = {Horario,ConfigHorario};
